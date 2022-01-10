be1d93bed7ee89f0a4ffdb9b1b61c57d5f898982
Griffin <sciontific1201@gmail.com>
2022-01-10 13:56:26 -0500

    Didnt add Program.cs oops

---
+++ gESilk/Program.cs
@@ -0,0 +1,16 @@
+﻿using gESilk.engine.window;
+
+namespace gESilk;
+
+public static class Program
+{
+    private static GameWindow _window;
+    
+    static void Main(string[] args)
+    {
+
+        _window = new(1280, 720, "garEngineSilk");
+        _window.Run();
+
+    }
+}
