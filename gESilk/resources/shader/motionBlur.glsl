613070229bae113d19b696c843c3964d0cc7b3e8
Griffin <sciontific1201@gmail.com>
2022-04-03 18:33:57 -0400

    Little motion blur improvements

--- gESilk/resources/shader/motionBlur.glsl
+++ gESilk/resources/shader/motionBlur.glsl
@@ -8,10 +8,6 @@ void main()
     gl_Position = vec4(inPos.x, inPos.y, 0.0, 1.0);
 }
     //-FRAGMENT-
-    /*
-      (C) 2020 David Lettier
-      lettier.com
-    */
     #version 330 core
 
 uniform sampler2D positionTexture;
@@ -24,35 +20,24 @@ uniform mat4 projection;
 out vec4 fragColor;
 
 void main() {
-    int   size       = 5;
-    float separation =     0.025;
+    int samples = 5;
 
-    vec2 texSize  = textureSize(colorTexture, 0).xy;
+    vec2 texSize = textureSize(colorTexture, 0).xy;
     vec2 texCoord = gl_FragCoord.xy / texSize;
 
     fragColor = texture(colorTexture, texCoord);
-    vec4 position1 = texture(positionTexture, texCoord);
+    vec4 prevPos = vec4(texture(positionTexture, texCoord).rgb, 1.0);
 
-    vec4 position0 = view * prevView * position1;
-    position0      = projection * position0;
-    position0.xy   = position0.xy * 0.5 + 0.5;
+    vec4 currentPos = view * prevView * prevPos;
+    currentPos = projection * currentPos;
+    currentPos /= currentPos.w;
 
-    position1      = projection * position1;
-    position1.xy   = position1.xy * 0.5 + 0.5;
+    prevPos = projection * prevPos;
+    prevPos /= prevPos.w;
+    vec2 direction = (prevPos.xy - currentPos.xy) * 0.025;
 
-    vec2 direction = position1.xy - position0.xy;
+    if (length(direction) <= 0.0) return;
+    for (int i = 0; i < samples; ++i, texCoord -= direction) fragColor += texture(colorTexture, texCoord);
 
-    if (length(direction) <= 0.0) { return; }
-
-    direction.xy *= separation;
-
-    vec2 forward  = texCoord;
-    vec2 backward = texCoord;
-
-    for (int i = 0; i < size; ++i) {
-        backward -= direction;
-        fragColor += texture(colorTexture, backward);
-    }
-
-    fragColor /= size + 1;
+    fragColor /= samples + 1;
 }
