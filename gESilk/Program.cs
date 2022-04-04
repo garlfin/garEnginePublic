40dff56ece1187ab2a00b25190e6c474c7ace1d0
Griffin <sciontific1201@gmail.com>
2022-04-04 13:30:32 -0400

    Removal of old packages and files.

--- gESilk/Program.cs
+++ gESilk/Program.cs
@@ -8,6 +8,11 @@ public static class Program
 
     private static void Main(string[] args)
     {
+        for (int i = 0; i < args.Length; i++)
+        {
+            args[i] = args[i].ToLower();
+        }
+
         MainWindow = new Application(1280, 720, "gE2");
         MainWindow.SetMotionBlur(!args.Contains("-noblur"));
         MainWindow.Run();
