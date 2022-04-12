d736d17750824ff7f8853b5cace7ef660320fd82
Griffin <sciontific1201@gmail.com>
2022-04-11 20:45:28 -0400

    Text non-functional, just uploading files for transfer

--- gESilk/engine/components/FontRenderer.cs
+++ gESilk/engine/components/FontRenderer.cs
@@ -1,4 +1,5 @@
-﻿using gESilk.engine.misc;
+﻿using System.Runtime.InteropServices;
+using gESilk.engine.misc;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
 
@@ -8,31 +9,63 @@ public class FontRenderer : Component
 {
     public Font Font;
 
-    public string Text;
+    private string _realText;
+
+    public string Text
+    {
+        get => _realText;
+        set
+        {
+            GetData(value, out var data, out var vtdata, out var ebodata);
+
+            GCHandle dataPinned = GCHandle.Alloc(data, GCHandleType.Pinned);
+            IntPtr dataPtr = dataPinned.AddrOfPinnedObject();
+            GCHandle vtPinned = GCHandle.Alloc(vtdata, GCHandleType.Pinned);
+            IntPtr vtPtr = vtPinned.AddrOfPinnedObject();
+            GCHandle eboPinned = GCHandle.Alloc(ebodata, GCHandleType.Pinned);
+            IntPtr eboPtr = eboPinned.AddrOfPinnedObject();
+
+            UpdateData(data.Length, dataPtr, vtPtr, ebodata.Length, eboPtr);
+
+            eboPinned.Free();
+            vtPinned.Free();
+            dataPinned.Free();
+
+            _realText = value;
+        }
+    }
 
     private readonly int _vbo, _vtvbo, _ebo, _vao, _elementLength;
 
     public FontRenderer(Font font, string text)
     {
         Font = font;
-        Text = text;
-        _vao = GL.GenVertexArray();
 
+        _vao = GL.GenVertexArray();
         GL.BindVertexArray(_vao);
 
-        _elementLength = 6 * Text.Length;
+        _elementLength = 6 * 100;
+        _vbo = GL.GenBuffer();
 
-        GetData(out var data, out var vtdata, out var ebodata);
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
+        GL.BufferData(BufferTarget.ArrayBuffer, 100 * 4 * 3 * sizeof(float), IntPtr.Zero, BufferUsageHint.DynamicDraw);
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+        GL.EnableVertexAttribArray(0);
 
-        _vbo = CreateBufferAttribute(GL.GenBuffer(), data, BufferUsageHint.StaticDraw, VertexAttribPointerType.Float,
-            0);
-        _vtvbo = CreateBufferAttribute(GL.GenBuffer(), vtdata, BufferUsageHint.StaticDraw,
-            VertexAttribPointerType.Float, 1);
+        _vtvbo = GL.GenBuffer();
+
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vtvbo);
+        GL.BufferData(BufferTarget.ArrayBuffer, 100 * 4 * 3 * sizeof(float), IntPtr.Zero, BufferUsageHint.DynamicDraw);
+        GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 0, 0);
+        GL.EnableVertexAttribArray(1);
 
         _ebo = GL.GenBuffer();
-        GL.BindBuffer(BufferTarget.ElementArrayBuffer, _ebo);
-        GL.BufferData(BufferTarget.ElementArrayBuffer, 6 * sizeof(int) * Text.Length, ebodata,
-            BufferUsageHint.StaticDraw);
+
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _ebo);
+        GL.BufferData(BufferTarget.ArrayBuffer, _elementLength * 3 * sizeof(int), IntPtr.Zero,
+            BufferUsageHint.DynamicDraw);
+
+        Text = text;
     }
 
     private static int CreateBufferAttribute(int buffer, Vector3[] data, BufferUsageHint usageArb,
@@ -52,33 +85,29 @@ public class FontRenderer : Component
         GL.DrawElements(PrimitiveType.Triangles, _elementLength, DrawElementsType.UnsignedInt, 0);
     }
 
-    public void UpdateText(string text)
+    public void UpdateData(int dataLength, IntPtr data, IntPtr vtdata, int eboDataLength, IntPtr ebodata)
     {
-        Text = text;
-
-        GetData(out var data, out var vtdata, out var ebodata);
-
         GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
-        //GL.BufferSubData(BufferTarget.ArrayBuffer, IntPtr.Zero, data);
+        GL.BufferSubData(BufferTarget.ArrayBuffer, IntPtr.Zero, dataLength, data);
 
         GL.BindBuffer(BufferTarget.ArrayBuffer, _vtvbo);
-        //GL.BufferSubData(BufferTarget.ArrayBuffer, sizeof(float) * vtdata.Length * 3, data, BufferUsageHint.DynamicDraw);
+        GL.BufferSubData(BufferTarget.ArrayBuffer, IntPtr.Zero, dataLength, vtdata);
 
         GL.BindBuffer(BufferTarget.ElementArrayBuffer, _ebo);
-        //GL.BufferSubData(BufferTarget.ElementArrayBuffer, 6 * sizeof(int) * Text.Length, ebodata, BufferUsageHint.DynamicDraw);
+        GL.BufferSubData(BufferTarget.ElementArrayBuffer, IntPtr.Zero, eboDataLength, ebodata);
     }
 
-    private void GetData(out Vector3[] data, out Vector3[] vtdata, out int[] ebodata)
+    private void GetData(string text, out Vector3[] data, out Vector3[] vtdata, out int[] ebodata)
     {
-        data = new Vector3[4 * Text.Length];
-        vtdata = new Vector3[4 * Text.Length];
-        ebodata = new int[6 * Text.Length];
+        data = new Vector3[4 * text.Length];
+        vtdata = new Vector3[4 * text.Length];
+        ebodata = new int[6 * text.Length];
 
         int vertexOffset = 0;
         int triOffset = 0;
         Vector3 cursorPos = Vector3.Zero;
 
-        foreach (var letter in Text)
+        foreach (var letter in text)
         {
             Character character = Font.Characters[0];
 
@@ -111,5 +140,11 @@ public class FontRenderer : Component
             vertexOffset += 4;
             triOffset += 6;
         }
+    }
+
+    ~FontRenderer()
+    {
+        GL.DeleteBuffers(3, new[] { _vbo, _vtvbo, _ebo });
+        GL.DeleteVertexArray(_vao);
     }
 }
